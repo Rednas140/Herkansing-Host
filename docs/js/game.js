c04fdb508c84cde5e5774bbb78bac788a150d63d
Sander <sanderjverhoeven@hotmail.com>
2021-08-19 20:03:39 +0200

    sounds fixed?

--- docs/js/game.js
+++ docs/js/game.js
@@ -11,11 +11,11 @@ class Game {
         this.chonker = [];
         this.spawn = false;
         this.spawnTimer = 0;
-        this.themeSong = new Audio("/docs/audio/song.mp3");
-        this.meow = new Audio("/docs/audio/meow.wav");
-        this.failHorn = new Audio("/docs/audio/failHorn.wav");
-        this.clapping = new Audio("/docs/audio/clapping.wav");
-        this.chonkerSound = new Audio("/docs/audio/chonker.mp3");
+        this.themeSong = new Audio("../docs/audio/song.mp3");
+        this.meow = new Audio("../docs/audio/meow.wav");
+        this.failHorn = new Audio("../docs/audio/failHorn.wav");
+        this.clapping = new Audio("../docs/audio/clapping.wav");
+        this.chonkerSound = new Audio("../docs/audio/chonker.mp3");
         this.themeSong.play();
         this.themeSong.volume = 0.5;
         this.themeSong.loop = true;
